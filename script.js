28edba4c1e2eebab377c2922f993e7fe5ed2eddb
Alexei <an@Alexeis-MacBook-Pro.local>
2023-11-19 23:44:29 +0200

    added comments

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let displayValue = '';
+let displayValue = ''; // Stores the current value shown on the display
 
+// Function to append values to the display when buttons are clicked
 function appendToDisplay(value) {
   displayValue += value;
   document.getElementById('display').value = displayValue;
 }
 
+// Function to clear the display
 function clearDisplay() {
   displayValue = '';
   document.getElementById('display').value = displayValue;
 }
 
+// Function to calculate and display the result
 function calculateResult() {
   try {
     displayValue = eval(displayValue);
